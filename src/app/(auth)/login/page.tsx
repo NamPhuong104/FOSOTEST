48e46832611cdc54e09d5f311d8d10ae2e3a2427
Frontend Developer <wowi.develop@gmail.com>
2025-03-21 12:03:37 +0700

    Add initial project setup with configuration files, UI components, and authentication pages

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login",
+};
+
+export default function Page() {
+  return (
+    <main className="flex h-screen items-center justify-center p-5">
+     
+    </main>
+  );
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,48 @@
+import { Toaster } from "@/components/ui/toaster";
+import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
+import localFont from "next/font/local";
+import ReactQueryProvider from "./ReactQueryProvider";
+
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+});
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Next15 Starter",
+    default: "Next15 Starter",
+  },
+  description: "Next15 Starter",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <ReactQueryProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </ReactQueryProvider>
+        <Toaster />
+      </body>
+    </html>
+  );
+}
